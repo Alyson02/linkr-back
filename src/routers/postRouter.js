ff922b1093d62e1bfaef57ba812f0ef4463fef96
Alyson02 <alyson.subrim2002@gmail.com>
2023-01-04 05:26:51 -0300

    feat: endpoint for list posts

--- src/routers/postRouter.js
+++ src/routers/postRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { create } from "../controllers/postController.js";
+import { create, list } from "../controllers/postController.js";
 import validationMiddleware from "../middlewares/validationMiddleware.js";
 import models from "../models/index.js";
 
 const postRouter = Router();
 
 postRouter.post("/add-post", validationMiddleware(models.postPOST), create);
+postRouter.get("/posts", list);
 
 export default postRouter;
