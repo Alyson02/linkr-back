3eb7f4f45c8750844d3f32da6f0960a389fefccd
Alyson02 <alyson.subrim2002@gmail.com>
2023-01-05 22:08:16 -0300

    fix: search urlMetadata

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,12 +1,23 @@
 import urlMetadata from "url-metadata";
+import { insertHashTag } from "../repositories/hashtagRepository.js";
 import { createPost, listPosts } from "../repositories/postRespository.js";
 
 export async function create(req, res) {
   try {
     const post = req.body;
     post.userId = 1; // res.locals.user.id;
-    console.log(post);
+
+    var regexp = /\B\#\w\w+\b/g;
+    const hashtags = post.content.match(regexp);
+
+    if (hashtags) {
+      hashtags.forEach(async (h) => {
+        await insertHashTag(h);
+      });
+    }
+
     await createPost(post);
+
     res.sendStatus(201);
   } catch (error) {
     res.status(500).send({
@@ -21,35 +32,37 @@ export async function list(req, res) {
   try {
     const posts = await listPosts();
 
-    const postsWithLinkMetaDatasUnresolved = posts.map(async (p) => {
+    const postsWithLinkMetaDatas = [];
+    for (let p of posts) {
       const metadata = {};
-
-      const res = await urlMetadata(p.link);
-
-      metadata.title = res.title;
-      metadata.description = res.description;
-      metadata.image = res.image;
       const url = p.link;
       delete p.link;
 
-      return {
-        ...p,
-        link: {
-          url,
-          ...metadata,
-        },
-      };
-    });
+      try {
+        const res = await urlMetadata(url);
 
-    const postsWithLinkMetaDatas = await Promise.all(
-      postsWithLinkMetaDatasUnresolved
-    );
+        metadata.title = res.title;
+        metadata.description = res.description;
+        metadata.image = res.image;
+
+        postsWithLinkMetaDatas.push({
+          ...p,
+          link: {
+            url,
+            ...metadata,
+            success: true,
+          },
+        });
+      } catch (error) {
+        postsWithLinkMetaDatas.push({ ...p, link: { url, success: false } });
+      }
+    }
 
     res.send(postsWithLinkMetaDatas);
   } catch (error) {
     res.status(500).send({
       success: false,
-      message: "Erro ao criar post",
+      message: "Erro ao listar posts",
       exception: error,
     });
   }
