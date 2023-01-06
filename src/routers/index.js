a721994d697e8cd3082c53c3c9e416e142e421cd
Alyson02 <alyson.subrim2002@gmail.com>
2023-01-06 20:33:48 -0300

    fix: router user

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import postRouter from "./postRouter.js";
 import authRouter from "./authRouter.js";
+import userRouter from "./userRouter.js";
 
 const router = Router();
 
 router.use(postRouter);
 router.use(authRouter);
+router.use(userRouter);
 
 export default router;
