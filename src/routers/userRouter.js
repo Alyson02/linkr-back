506afc2809d0b2df8f1a6f94b99a971cecb76588
Barbara Luz <barbaraluz@eq.ufrj.br>
2023-01-06 22:59:05 -0300

    feat: auth validation

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getUser, searchUser } from "../controllers/userController.js";
+import authValidation from "../middlewares/authValidationMiddleware.js";
 import { validationUser, validationUserName } from "../middlewares/userMiddleware.js";
 
 const router = Router()
 
-router.get('/user/:id', validationUser, getUser)
-router.get('/user/search/:name', validationUserName, searchUser)
+router.get('/user/:id', authValidation, validationUser, getUser)
+router.get('/user/search/:name', authValidation, validationUserName, searchUser)
 
 export default router
